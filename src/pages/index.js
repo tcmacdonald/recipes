b8b4ab5b78617627f2dc98a79f4eaf22165d5982
Taylor C. MacDonald <tcmacdonald@gmail.com>
2020-02-02 10:29:17 -0500

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,40 @@
-import React from "react"
+import React, { Component } from 'react'
+import { graphql } from 'gatsby'
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default class index extends Component {
+  recipes() {
+    return this.props.data.allMarkdownRemark.edges.map(edge => edge.node).map((node,i) => {
+      return (<li key={i}><Link to={`/recipes/${node.frontmatter.slug}`}>{node.frontmatter.title}</Link></li>)
+    })
+  }
 
-export default IndexPage
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <ol>{this.recipes()}</ol>
+      </Layout>
+    );
+  }
+}
+
+export const indexQuery = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+            image_url
+            source_name
+            source_url
+          }
+        }
+      }
+    }
+  }
+`
--- src/templates/recipeTemplate.js
+++ src/templates/recipeTemplate.js
@@ -1,25 +1,32 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function Template(props) {
   const { markdownRemark } = props.data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
 
-  console.log(frontmatter)
   const ingredients = (frontmatter.ingredients || []).map((el,i) => {
     return (<li key={i}>{el}</li>)
   })
   return (
-    <div className="recipe-container">
-      <div className="recipe">
-        <h1>{frontmatter.title}</h1>
-        <ol>{ingredients}</ol>
-        <div
-          className="recipe-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <Layout>
+      <SEO title="Recipe" />
+      <div className="recipe-container">
+        <div className="recipe">
+          <h1>{frontmatter.title}</h1>
+          <p><a href={frontmatter.source_url}>{frontmatter.source_url}</a></p>
+          <hr />
+          <ol>{ingredients}</ol>
+          <hr />
+          <div
+            className="recipe-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
@@ -30,6 +37,8 @@ export const pageQuery = graphql`
       frontmatter {
         title
         ingredients
+        source_name
+        source_url
       }
     }
   }
