8561f09395aba979d58b8d3845e389aee1ffffc9
Taylor C. MacDonald <tcmacdonald@gmail.com>
2020-12-30 13:42:22 -0500

    Update metatags

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,6 +67,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `viewport`,
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
       ].concat(meta)}
     />
   )
